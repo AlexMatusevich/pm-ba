6161175dfcdeb7b4757e6626b846661baa113af9
niizom <nizomabdumalikov@gmail.com>
2024-05-03 16:34:43 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Documentation Project for PM and BA
+Welcome to the Documentation website! This site serves as the central hub for project managers and business analytics staff to access comprehensive guides, tutorials, and resources that will help you effectively manage your projects and analyze business data using the Astro platform.
+
+## Getting Started
+
+install all the dependencies by running the following command:
+```bash
+npm install
+```
+
+then start the development server by running:
+```bash
+npm run dev
+```
---
+++ astro.config.mjs
@@ -0,0 +1,49 @@
+import { defineConfig, passthroughImageService } from 'astro/config';
+
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://AlexMatusevich.github.io',
+  base: 'pm-ba',
+  image: {
+    service: passthroughImageService(),
+  },
+  integrations: [starlight({
+    favicon: '/favicon.ico',
+    title: 'Vention Document',
+    logo: {
+      light: './src/assets/icons/logo-dark.svg',
+      dark: './src/assets/icons/logo-light.svg',
+      replacesTitle: true,
+    },
+    expressiveCode: {
+      useStarlightDarkModeSwitch: true
+    },
+    pagination: false,
+    customCss: ['./src/styles/index.css'],
+    sidebar: [
+      {
+        label: "PAs knowledge base",
+        items: [
+          {
+            label: "Knowledge areas",
+            autogenerate: { directory: "pm/knowledge-areas" },
+          },
+          {
+            label: 'Resources',
+            autogenerate: { directory: "pm/resources" },
+          },
+          {
+            label: 'Certifications/Courses',
+            autogenerate: { directory: "pm/courses" },
+          }
+        ],
+      },
+      {
+        label: "BAs knowledge base",
+        autogenerate: { directory: "reference" },
+      },
+    ],
+  })]
+});
